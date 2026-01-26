3a7bc2e60c3a05d7dbd6d99338c8b5745ee2dceb
danny25lara <angeldaniellara25@gmail.com>
2026-01-26 15:21:11 -0500

    Refactor Firebase module import statements

--- js/module/firebase.js
+++ js/module/firebase.js
@@ -1,22 +1,22 @@
-//module/firebase.js
-// 1. Importamos las librerías desde la Nube (CDN)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// 2. Tu Configuración (La que me diste)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCoe10X5SuSXNVlTI4zcaRSRC_u4z7rV-o",
-    authDomain: "gestor-de-proyectos-academicos.firebaseapp.com",
-    projectId: "gestor-de-proyectos-academicos",
-    storageBucket: "gestor-de-proyectos-academicos.firebasestorage.app",
-    messagingSenderId: "268011164638",
-    appId: "1:268011164638:web:2eeb93c7cc4d12e74e834f"
+  apiKey: "AIzaSyCoe10X5SuSXNVlTI4zcaRSRC_u4z7rV-o",
+  authDomain: "gestor-de-proyectos-academicos.firebaseapp.com",
+  projectId: "gestor-de-proyectos-academicos",
+  storageBucket: "gestor-de-proyectos-academicos.firebasestorage.app",
+  messagingSenderId: "268011164638",
+  appId: "1:268011164638:web:2eeb93c7cc4d12e74e834f"
 };
 
-// 3. Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // 4. Exportar las herramientas para usarlas en otros archivos
 export const db = getFirestore(app); // Base de datos
 export const auth = getAuth(app);    // Sistema de usuarios
+
